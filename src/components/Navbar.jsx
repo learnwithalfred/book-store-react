45d0242d78c41e1b7c7cdc063db319faca7dad1c
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-06 22:57:17 +0000

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+      <a className="navbar-brand" href="/">
+        BookStore CMS
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Books
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Categories
+            </a>
+          </li>
+        </ul>
+        <a
+          href="/"
+          className="d-block link-dark text-decoration-none"
+        >
+          <img
+            src="https://github.com/mdo.png"
+            alt="mdo"
+            width="32"
+            height="32"
+            className="rounded-circle"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
