e093d7e968ac1a623194dcda6837c780216530e1
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-07 08:54:21 +0000

    Add progress bar to book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Book.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book() {
+  const percentage = 64;
   return (
     <div className="book-container">
       <div className="card-body">
         <div className="row">
           <div className="book-data col-6">a</div>
-          <div className="book-progress col-3">b</div>
+          <div className="book-progress col-3">
+            <div className="Rectangle-3">
+              <div style={{ width: 50, height: 50, marginRight: 5 }}>
+                <CircularProgressbar
+                  value={percentage}
+                  text={`${percentage}%`}
+                />
+              </div>
+            </div>
+            <div className="percentage">
+              <span className="-Percent-Complete">{`${percentage}%`}</span>
+              <span className="Completed Text-Style-2">Completed</span>
+            </div>
+          </div>
           <div className="book-chapter col-3">
             <span className="chapter-description">Current Chapter</span>
             <span className="Lesson-Panel">Chapter 17</span>
