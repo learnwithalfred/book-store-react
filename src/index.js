a3303045dbe14ac959c666c107a776c61b1b9619
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-07 11:15:34 +0000

    Link category page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Books from './routes/Books';
+import Category from './routes/Category';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Books />} />
+      <Route path="/category" element={<Category />} />
     </Routes>
   </BrowserRouter>,
 );
