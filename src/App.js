236648f916577ca266550efc252e8966ebaf9649
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-07 11:16:00 +0000

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import AddBookForm from './components/Add-book-form';
-import Book from './components/Book';
-import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Book />
-      <Book />
-      <AddBookForm />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
