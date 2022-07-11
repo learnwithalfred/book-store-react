830a7d42f5c1e67770f92a16be25509a553a9e32
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-11 20:31:40 +0000

    Add status dispatch

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,25 +1,52 @@
-const initialState = [
-  {
-    id: '1',
-    name: 'Sports',
-  },
-  {
-    id: '456789',
-    name: 'Entertainment',
-  },
-  {
-    id: '4569876789',
-    name: 'Education',
-  },
-];
+const initialState = {
+  categories: [
+    {
+      id: '1',
+      name: 'Sports',
+    },
+    {
+      id: '456789',
+      name: 'Entertainment',
+    },
+    {
+      id: '4569876789',
+      name: 'Education',
+    },
+    {
+      id: 'kads8798',
+      name: 'Music',
+    },
+  ],
+  cur_status: 'Under Construction',
+};
+
+export const checkStatus = () => ({
+  type: 'CHECK_STATUS',
+});
+
+export const getCategories = () => ({
+  type: 'GET_CATEGORIES',
+});
 
 const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'CHECK_STATUS':
+      return {
+        ...state,
+        cur_status: state.cur_status,
+      };
+    case 'GET_CATEGORIES':
+      return {
+        ...state,
+        categories: state.categories,
+      };
+
     default:
       return state;
   }
 };
 
-export const selectAllCategories = (state) => state.categories;
+export const selectAllCategories = (state) => state.categories.categories;
+export const getStatus = (state) => state.categories.cur_status;
 
 export default categoryReducer;
--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from '../components/Navbar';
+import { getStatus } from '../redux/categories/categories';
 
 function Category() {
+  const [appStatus, setAppStatus] = useState('');
+  const curStatus = useSelector(getStatus);
+
+  const loadStatus = () => {
+    setAppStatus(appStatus === '' ? curStatus : '');
+  };
+
   return (
     <>
       <Navbar />
       <div className="books-container">
-        <button type="button" className="btn btn-primary">
+        <button
+          onClick={() => loadStatus()}
+          type="button"
+          className="btn btn-primary"
+        >
           Check status
         </button>
+        <h1 className="mx-4 my-4">{appStatus}</h1>
       </div>
     </>
   );
