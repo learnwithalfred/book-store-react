c2656deaef0d74dde72a7bf004028969df2774dc
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-12 22:22:51 +0000

    Add thunk to project

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+// import { configureStore } from '@reduxjs/toolkit';
+// import bookReducer from './books/books';
+// import categoryReducer from './categories/categories';
+
+// const store = configureStore({
+//   reducer: {
+//     books: bookReducer,
+//     categories: categoryReducer,
+//   },
+// });
+// export default store;
+import {
+  combineReducers,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-const store = configureStore({
-  reducer: {
-    books: bookReducer,
-    categories: categoryReducer,
-  },
+const rootReducer = combineReducers({
+  books: bookReducer,
+  categories: categoryReducer,
 });
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default store;
