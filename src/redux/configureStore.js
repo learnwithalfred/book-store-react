0079085501511b0680dd1dae40b98aca908ecc73
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-13 08:56:31 +0000

    Create store using configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,12 @@
-import {
-  combineReducers,
-  legacy_createStore as createStore,
-  applyMiddleware,
-} from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-const rootReducer = combineReducers({
-  books: bookReducer,
-  categories: categoryReducer,
+const store = configureStore({
+  reducer: {
+    books: bookReducer,
+    categories: categoryReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
