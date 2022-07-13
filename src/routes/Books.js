bf2dd962686b800d4fd7c5f264232c559a4ffeea
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-13 13:12:27 +0000

    Render data dynamically using thunk

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -3,21 +3,30 @@ import { useSelector, useDispatch } from 'react-redux';
 import AddBookForm from '../components/Add-book-form';
 import Book from '../components/Book';
 import Navbar from '../components/Navbar';
-import { getBooks } from '../redux/books/books';
+import {
+  fetchBooks,
+  selectAllBooks,
+  getBooksStatus,
+  getBooksError,
+} from '../redux/books/books';
 
 function Books() {
-  const { books, loading } = useSelector((state) => state.books);
   const dispatch = useDispatch();
+  const books = useSelector(selectAllBooks);
+  const bookStatus = useSelector(getBooksStatus);
+  const error = useSelector(getBooksError);
 
   useEffect(() => {
-    dispatch(getBooks());
-  }, [dispatch]);
+    if (bookStatus === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [bookStatus, dispatch]);
 
-  let fetchedBooks = '';
-  if (loading) {
-    fetchedBooks = <h2>Loading...</h2>;
-  } else {
-    fetchedBooks = books.map((book) => (
+  let booksContent = '';
+  if (bookStatus === 'loading') {
+    booksContent = <h2>Loading...</h2>;
+  } else if (bookStatus === 'succeeded') {
+    booksContent = books.map((book) => (
       <div key={book.item_id} className="container">
         <Book
           id={book.item_id}
@@ -28,13 +37,15 @@ function Books() {
         />
       </div>
     ));
+  } else if (bookStatus === 'failed') {
+    booksContent = <p>{error}</p>;
   }
 
   return (
     <>
       <Navbar />
       <div className="books-container">
-        {fetchedBooks}
+        {booksContent}
         <hr className="line-break" />
         <AddBookForm />
       </div>
