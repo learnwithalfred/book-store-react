1d6cc93019faeb372459f839b7c0847d79b31332
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-14 09:04:15 +0000

    Update solution during mob programming

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Navbar from '../components/Navbar';
-import { CheckStatus } from '../redux/categories/categories';
 
 function Category() {
   const categories = useSelector((state) => state.categories.categories);
-  const dispatch = useDispatch();
-  const update = (event) => {
-    event.preventDefault();
-
-    dispatch(CheckStatus());
-  };
 
   return (
     <>
       <Navbar />
       <div className="books-container">
-        <button type="button" className="btn btn-primary" onClick={update}>
+        <button type="button" className="btn btn-primary">
           Check status
         </button>
         <h1 className="my-4 display-1">{categories}</h1>
